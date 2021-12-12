d77b0b01fdfd81338d450524d444dab9ba079e72
nick <nick4supreme@gmail.com>
2021-12-13 03:00:48 +0800

    spring-cloud-stream

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/nik/springcloud/StreamMQMain8803.java
@@ -0,0 +1,19 @@
+package com.nik.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author Nick4Supreme
+ * @date 2021/12/13
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class StreamMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+
+}
