88bd939f5196a65eabc8040857f58565511082de
nick <nick4supreme@gmail.com>
2021-12-15 08:00:57 +0800

    spring-cloud-alibaba nacos

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/nick/springcloud/OrderNacosMain83.java
@@ -0,0 +1,17 @@
+package com.nick.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Nick4Supreme
+ * @date 2021/12/14
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class,args);
+    }
+}
