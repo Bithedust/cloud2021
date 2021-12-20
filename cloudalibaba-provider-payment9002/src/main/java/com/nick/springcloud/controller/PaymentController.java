c59358a7358e485d4387621d32c0f6be608508b4
nick <nick4supreme@gmail.com>
2021-12-20 20:24:54 +0800

    spring-cloud-alibaba nacos

--- cloudalibaba-provider-payment9002/src/main/java/com/nick/springcloud/controller/PaymentController.java
+++ cloudalibaba-provider-payment9002/src/main/java/com/nick/springcloud/controller/PaymentController.java
@@ -1,6 +1,8 @@
 package com.nick.springcloud.controller;
 
+import jdk.nashorn.internal.ir.annotations.Reference;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +17,11 @@ public class PaymentController {
     @Value("${server.port}")
     private String serverPort;
 
+
     @GetMapping("/payment/nacos/{id}")
     public String getPayment(@PathVariable("id") Integer id){
 
-        return "nacos registry, server port:" + serverPort + ", id:" + id;
+        return "nacos registry, server port:" + serverPort + ", id:" + id ;
 
     }
 
