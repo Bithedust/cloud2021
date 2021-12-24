3545fd84343f557a28ed44aa119e6ff056dc48a0
nick <nick4supreme@gmail.com>
2021-12-24 23:02:08 +0800

    spring cloud alibaba sentinel

--- cloudalibaba-consumer-nacos-order84/src/main/java/com/nick/springcloud/controller/CircleBreakerController.java
+++ cloudalibaba-consumer-nacos-order84/src/main/java/com/nick/springcloud/controller/CircleBreakerController.java
@@ -6,14 +6,12 @@ import com.nick.springcloud.entities.CommonResult;
 import com.nick.springcloud.entities.Payment;
 import com.nick.springcloud.service.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import javax.annotation.Resource;
 import java.util.Objects;
 
 /**
